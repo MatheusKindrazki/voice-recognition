e9fd35e84a93dcdb8c973813012432f1c0cdf471
Matheus Kindrazki <matheuskindrazki@gmail.com>
2021-07-10 11:53:34 -0300

    feat: read out loud

--- app.js
+++ app.js
@@ -14,9 +14,22 @@ recognition.onresult = function (event) {
 
   content.textContent = transcript;
 
+  readOutLoud(transcript)
+
 }
 
 // ! Add the listener to the button 
 button.addEventListener('click', () => {
   recognition.start()
 })
+
+
+function readOutLoud(message) {
+  const speech = new SpeechSynthesisUtterance();
+  speech.text = message
+  speech.volume = 1;
+  speech.rate = 1;
+  speech.pitch = 1;
+
+  window.speechSynthesis.speak(speech)
+}
